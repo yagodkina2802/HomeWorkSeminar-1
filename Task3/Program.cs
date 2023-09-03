d4b5691b402441d4101443c49a8bc4815ec1d151
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-09-03 23:25:28 +0300

    Завершающий коммит

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int lastDigit = number%10;
+if(lastDigit == 0)
+{
+    Console.WriteLine("Да");
+}
