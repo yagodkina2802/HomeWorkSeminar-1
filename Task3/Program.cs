568dc3db3846488c9655e674f162c66b6375196c
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-09-04 10:14:11 +0300

    Решение 3 задачи

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,7 +7,11 @@
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 int lastDigit = number%10;
-if(lastDigit == 0)
+if(lastDigit == 0 ||lastDigit == 2 || lastDigit == 4 || lastDigit == 6 || lastDigit == 8)
 {
     Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
 }
